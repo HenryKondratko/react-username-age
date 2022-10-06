738cb63e83ea808945c39d90343b80a2f70722a9
Henry Kondratko <henry.kondratko@gmail.com>
2022-10-06 16:33:36 -0400

    ErrorModal component and styling

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from "./Card";
+import Button from "./Button";
+import classes from './ErrorModal.module.css'
+
+const ErrorModal = props => {
+    return (
+        <div>
+            <div className={classes.backdrop} />
+            <Card className={classes.modal}>
+                <h2 className={classes.header}>{props.title}</h2>
+                <div className={classes.content}>
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <Button>Okay</Button>
+                </footer>
+            </Card>
+        </div>
+    )
+};
+
+export default ErrorModal
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -30,6 +31,8 @@ const AddUser = props => {
     };
 
     return (
+        <div>
+        <ErrorModal title="An error occurred!" message="Something went wrong!" />
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
@@ -39,6 +42,7 @@ const AddUser = props => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
+        </div>
     );
 };
 
