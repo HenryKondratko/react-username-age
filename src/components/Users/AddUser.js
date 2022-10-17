9fe171936f85f11a726a5e80880a40306b932131
Henry Kondratko <henry.kondratko@gmail.com>
2022-10-17 10:14:06 -0400

    wrapper component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -19,7 +20,7 @@ const AddUser = props => {
             });
             return;
         }
-        if(+enteredAge < 1){
+        if (+enteredAge < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0).',
@@ -44,18 +45,18 @@ const AddUser = props => {
     };
 
     return (
-        <div>
-            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
-        </div>
+        <Wrapper>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </Wrapper>
     );
 };
 
